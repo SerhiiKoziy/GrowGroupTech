cdf57e3ca798855e7816fc13175535bf24f21b7a
SerhiiKoziyReflaunt <serhii.koziy@reflaunt.com>
2023-11-01 15:10:45 +0200

    updated port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
